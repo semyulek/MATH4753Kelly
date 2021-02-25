7be8d8e646ce597b4e9df3806cc8d24a926b86bc
Samuel Kelly <semyulek@ou.edu>
2021-02-25 11:54:04 -0600

    Binomial function

---
+++ R/mybin.R
@@ -0,0 +1,29 @@
+#' @title A binomial function
+#'
+#' @param iter
+#' @param n
+#' @param p
+#'
+#' @return Returns a table of the binomial values, and returns a graph illustrating the number of successes of the simulation
+#' @export
+#'
+#' @examples
+#' \dontrun{mybin(iter = 10000, n = 20, p = .9)}
+mybin=function(iter=100,n=10, p=0.5){
+  # make a matrix to hold the samples
+  #initially filled with NA's
+  sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
+  #Make a vector to hold the number of successes in each trial
+  succ=c()
+  for( i in 1:iter){
+    #Fill each column with a new sample
+    sam.mat[,i]=sample(c(1,0),n,replace=TRUE, prob=c(p,1-p))
+    #Calculate a statistic from the sample (this case it is the sum)
+    succ[i]=sum(sam.mat[,i])
+  }
+  #Make a table of successes
+  succ.tab=table(factor(succ,levels=0:n))
+  #Make a barplot of the proportions
+  barplot(succ.tab/(iter), col=rainbow(n+1), main="Binomial simulation", xlab="Number of successes")
+  succ.tab/iter
+}
