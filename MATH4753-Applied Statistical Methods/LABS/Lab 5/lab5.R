a385d6e360c89e47f84718894440ea3f2eca181c
Samuel Kelly <semyulek@ou.edu>
2021-02-25 10:13:32 -0600

    Task 2 complete

--- MATH4753-Applied Statistical Methods/LABS/Lab 5/lab5.R
+++ MATH4753-Applied Statistical Methods/LABS/Lab 5/lab5.R
@@ -19,7 +19,7 @@ sample(B,size=20,prob=c(1/5,1/5,1/5,1/5,1/5),replace=TRUE)
 ## sampling function
 # iter = iterations, n=sample size
 # set default values
-mybin=function(iter=100,n=10, p=0.5){ 
+mybin=function(iter=100,n=10, p=0.5){
 # make a matrix to hold the samples
 #initially filled with NA's
 sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
@@ -37,11 +37,12 @@ succ.tab=table(factor(succ,levels=0:n))
 barplot(succ.tab/(iter), col=rainbow(n+1), main="Binomial simulation", xlab="Number of successes")
 succ.tab/iter
 }
-mybin(iter=1000,n=18, p=0.3)
+obj = mybin(iter=10000,n=18, p=0.3)
 
+round(dbinom(0:18,18,.3),4)
 ## Try a multinomial
 
-mymult=function(iter=100,n=10, p=c(1,1,1,1)/4){ 
+mymult=function(iter=100,n=10, p=c(1,1,1,1)/4){
 # make a matrix to hold the samples
 #initially filled with NA's
 sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
@@ -54,7 +55,7 @@ tab.mat=matrix(NA,nr=k,nc=iter, byrow=TRUE)
 for(i in 1:iter){
 #Fill each column with a new sample
 sam.mat[,i]=sample(1:k,n,replace=TRUE, prob=p)
-#Collect all the frequencies of each of the k values 
+#Collect all the frequencies of each of the k values
 tab.mat[,i]=table(factor(sam.mat[,i],levels=1:k))
 }
 # sum the frequecies
@@ -83,7 +84,7 @@ s=sample(1:10,n,replace=TRUE)
 # turn the sample into a factor
 sf=factor(s,levels=1:10)
 #make a barplot
-barplot(table(sf)/n,beside=TRUE,col=rainbow(10), 
+barplot(table(sf)/n,beside=TRUE,col=rainbow(10),
 main=paste("Example sample()", " iteration ", i, " n= ", n,sep="") ,
 ylim=c(0,0.2)
 )
