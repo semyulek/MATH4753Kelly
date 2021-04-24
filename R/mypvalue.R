5758ec6463b07ff47016962880efebf2f68013ae
Samuel Kelly <semyulek@ou.edu>
2021-04-23 20:15:38 -0500

    mypvalue function

---
+++ R/mypvalue.R
@@ -0,0 +1,46 @@
+#' @title P value area graph
+#'
+#' @param t0 The calculation of our t statistic.
+#' @param xmax The x range of values that we want to be graphed.
+#' @param n The size of our sample.
+#' @param alpha The confidence level 1-alpha.
+#'
+#' @return Returns the p-value and t statistic, as well as a graph of the region where we reject the null hypothesis indicated by two vertical lines.
+#' @export
+#'
+#' @examples
+# Display P-value areas
+mypvalue=function(t0,xmax=4,n=20, alpha=0.05){
+  #calculate alpha/2
+  va=round(pt(-t0,df=n-1),4)
+  pv=2*va
+
+  # plot the t dist
+  curve(dt(x,df=n-1),xlim=c(-xmax,xmax),ylab="T Density",xlab=expression(t),
+        main=substitute(paste("P-value=", pv, " alpha=", alpha)))
+
+
+  # set up points on the polygon to the right
+  xcurve=seq(t0,xmax,length=1000)
+  ycurve=dt(xcurve,df=n-1)
+
+  # set up points to the left
+  xlcurve=seq(-t0,-xmax,length=1000)
+  ylcurve=dt(xcurve,df=n-1)
+
+  # Shade in the polygon defined by the line segments
+  polygon(c(t0,xcurve,xmax),c(0,ycurve,0),col="green")
+  polygon(c(-t0,xlcurve,-xmax),c(0,ylcurve,0),col="green")
+
+  # make quantiles
+  q=qt(1-alpha/2,n-1)
+  abline( v=c(q,-q),lwd=2) # plot the cut off t value
+  axis(3,c(q,-q),c(expression(abs(t[alpha/2])),expression(-abs(t[alpha/2]))))
+
+
+  # Annotation
+  text(0.5*(t0+xmax),max(ycurve),substitute(paste(area, "=",va)))
+  text(-0.5*(t0+xmax),max(ycurve),expression(area))
+
+  return(list(q=q,pvalue=pv))
+}
